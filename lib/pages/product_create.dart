f831ed217442079357707048b6e78639876467bd
phemmelliot <adegokefemi17@gmail.com>
2019-05-17 19:58:14 +0100

    Use bottom navigation modal sheet and wrapped up on navigation

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Product List Page'),);
+    return Center(
+      child: RaisedButton(
+        child: Text('Show Modal'),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Text(
+                    'Here is the bottom navigaiton sheet',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                );
+              });
+        },
+      ),
+    );
   }
 }
