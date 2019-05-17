eb52ce81fdc7ba7a0f414517a300283d1f11ec0d
phemmelliot <adegokefemi17@gmail.com>
2019-05-17 17:56:59 +0100

    Use named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import './pages/auth.dart';
+import 'pages/product_admin.dart';
+import 'pages/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple,
-        ),
-        home: AuthPage());
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple,
+      ),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => ProductsPage(),
+        '/admin': (BuildContext context) => ProductsAdminPage()
+      },
+    );
   }
 }
